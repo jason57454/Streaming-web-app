17511ba991ed4da296a7d5ccc3c843cfa12bdf62
jason <jason.chambrin@mailo.com>
2023-03-20 22:49:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ window.addEventListener("load", () => {
     for (let i = 0; i < tab.length; i++) {
       tab[i].style.display = "none";
       if (tab[i].dataset.path === path) {
-        console.log(tab);
         tab[i].style.display = "flex";
         loader.style.opacity = "0";
       }
@@ -184,7 +183,7 @@ function addBookmarks(event) {
   if (index !== -1) {
     const cardData = jsonData[index];
     const isBookmarked = !bookmarkState[cardId]; // inverse l'état actuel de signet
-    console.log(isBookmarked);
+
     signet.classList.toggle("bookmark_active", isBookmarked);
     cardData.isBookmarked = isBookmarked;
     bookmarkState[cardId] = isBookmarked ?? false; // stocke l'état de signet dans bookmarkState
@@ -206,14 +205,28 @@ function displaySearch(
   let resultMessage = document.querySelector(resultMessageClass);
   let displayResult = document.querySelector(displayResultClass);
   let currentTabId = window.location.hash;
+  const bookmark = document.querySelectorAll(".bookmark");
 
-  console.log(currentTabId);
+  // calling addBookmarks function
+  bookmark.forEach((bookmark) => {
+    const cardId = parseInt(bookmark.closest(".card").dataset.cardId);
+    const isBookmarked = bookmarkState[cardId] ?? false;
+
+    bookmark
+      .querySelector(".bookmark_icon_1")
+      .classList.toggle("bookmark_active", !isBookmarked);
+    bookmark
+      .querySelector(".bookmark_icon_2")
+      .classList.toggle("bookmark_active", isBookmarked);
+    bookmark.addEventListener("click", addBookmarks);
+  });
+
   input.addEventListener("keyup", function () {
-    console.log(bookmarkState);
     onSearch.style.display = "none";
     result.style.display = "flex";
     value = input.value.trim();
     resultMessage.innerHTML = "";
+    console.log(bookmarkState);
 
     const filteredData = jsonData.filter((item) => {
       if (currentTabId === "#/movie") {
@@ -249,6 +262,7 @@ function displaySearch(
 
     if (filteredData.length > 0) {
       displayResult.style.display = "flex";
+
       displayResult.innerHTML = filteredData
         .map((card) => {
           return ` 
@@ -277,7 +291,9 @@ function displaySearch(
               fill="none"
             />
           </svg>
-          <svg class="bookmark_icon bookmark_icon_2" width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z" fill="#FFF"/></svg>
+          <svg class="bookmark_icon bookmark_icon_2 ${
+            bookmarkState[card.id] === true ? "bookmark_active" : ""
+          }" width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z" fill="#FFF"/></svg>
         </div>
       </div>
       <div class="col col_6">
@@ -321,7 +337,7 @@ function displayTrendingCards() {
             class="opacity_hover"
             src="${card.thumbnail.trending.small}"
             alt=""
-          />
+          /> 
           <div class="play_card row">
                 <img src="./assets/icon-play.svg" alt="" />
                 <p class="medium">Play</p>
@@ -567,8 +583,6 @@ function displayBookmarksCards() {
   const bookmarkedCards = jsonData.filter(
     (card) => cardIds.indexOf(card.id.toString()) !== -1
   );
-  console.log(cardIds);
-  console.log(bookmarkedCards);
 
   bookmarkedMovies.innerHTML = bookmarkedCards
     .filter((card) => card.category === "Movie")
